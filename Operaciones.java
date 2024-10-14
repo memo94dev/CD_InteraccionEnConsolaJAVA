cd147d2c5138587aa595042834ddcb73fbaac72b
Guillermo Home <guille.work.94@gmail.com>
2024-10-14 17:24:00 -0300

    Primer commit

---
+++ Operaciones.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Operaciones {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        System.out.println("Ingrese un numero para una de las siguientes opciones: ");
+        System.out.println("1- Calcular el área de un círculo.");
+        System.out.println("2- Calcular el área de un cuadrado.");
+        System.out.println("3- Calcular el área de un triángulo.");
+        System.out.println("4- Calcular el factorial de un número.");
+        System.out.println("5- Determinar si un número es par o impar.");
+        System.out.println("6- Determinar si un número es par o impar.");
+        System.out.println("7- Calcular la suma de todos los números desde 1 hasta un número dado.");
+        System.out.println("8- Salir!");
+
+        
+
+    }
+}
